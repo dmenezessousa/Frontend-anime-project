7de0dbfa63dd15c2a91d1117b105ae446fd8977a
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-12-14 18:23:39 -0500

    created a hook for search input

--- src/components/Anime/Anime.js
+++ src/components/Anime/Anime.js
@@ -2,8 +2,25 @@ import React from "react";
 import Row from "../Row/Row";
 import Banner from "../Banner/Banner";
 import requests from "../lib/requests";
-
+import AnimeHooks from "../lib/animeHooks";
 function Anime() {
+  let [animeInput, setAnimeInput, ,] = AnimeHooks();
+
+  function getRandomAnime() {
+    let randomMoviesArray = ["naruto", "onepiece", "fullmetal", "gintama"];
+
+    let randomIndex = Math.floor(Math.random() * randomMoviesArray.length);
+
+    return randomMoviesArray[randomIndex];
+  }
+
+  const requests = {
+    fetchTv: `/search/anime?type=tv&q=${getRandomAnime()}`,
+    fetchMovies: `/search/anime?type=Movie&q=${getRandomAnime()}`,
+    fetchSpecial: `/search/anime?type=Special&q=${getRandomAnime()}`,
+    fetchOVA: `/search/anime?type=OVA&q=${getRandomAnime()}`,
+  };
+
   return (
     <div>
       <Banner />
---
+++ src/components/lib/animeHooks.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+
+function AnimeHooks() {
+  const [animeInput, setAnimeInput] = useState("");
+  const [animeArray, setAnimeArray] = useState([]);
+  const [submit, setSubmit] = useState(false);
+  const { search } = useLocation();
+  const navigate = useNavigate();
+
+  function getRandomAnime() {
+    let randomMoviesArray = ["naruto", "onepiece", "fullmetal", "gintama"];
+
+    let randomIndex = Math.floor(Math.random() * randomMoviesArray.length);
+
+    return randomMoviesArray[randomIndex];
+  }
+
+  async function getAnime(animeTitle) {
+    navigate(`/anime?q=${animeTitle}`, {
+      replace: true,
+    });
+    try {
+      let payload = await axios.get(
+        `https://api.jikan.moe/v3/search/anime?q=${animeTitle}`
+      );
+
+      let animeIdArray = payload.data.Search.map((item) => item.imdbID);
+
+      let promiseAnimeArray = animeIdArray.map(async (item) => {
+        return await axios.get(
+          `https://api.jikan.moe/v3/search/anime?q=${item}`
+        );
+      });
+
+      Promise.all(promiseAnimeArray)
+        .then((result) => {
+          setAnimeArray(result);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } catch (e) {}
+  }
+
+  useEffect(() => {
+    if (submit) {
+      setSubmit(false);
+      getAnime(animeInput);
+    }
+  }, [submit]);
+
+  useEffect(() => {
+    const values = queryString.parse(search);
+    if (values.q) {
+      getAnime(values.q);
+    } else {
+      getAnime(getRandomAnime());
+    }
+  }, []);
+  return [animeInput, setAnimeInput, animeArray, setSubmit];
+}
+
+export default AnimeHooks;
